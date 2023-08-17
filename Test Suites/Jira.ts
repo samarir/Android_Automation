7deaa6825494247eaa8613a2190a1313522515c0
samarir <samarir@masaratapp.com>
2023-08-17 11:42:41 +0000

    15th commit

---
+++ Test Suites/Jira.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jira</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6b665ed6-ed2d-4304-8873-47b07c799103</testSuiteGuid>
+   <testCaseLink>
+      <guid>27428d3b-4ce0-463d-b8e0-f80423911daf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Managemenet/EK-8058 Katalon - login (integration test)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
