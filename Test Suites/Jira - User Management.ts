40841681794af1baa4096520822ea19a9ea846b2
samarir <samarir@masaratapp.com>
2023-09-23 15:55:52 +0000

    38th commit

---
+++ Test Suites/Jira - User Management.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jira - User Management</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d1ff3261-4de1-48e7-90af-95d565994b2c</testSuiteGuid>
+   <testCaseLink>
+      <guid>000d2649-8be9-4449-84bf-647cc505a5d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EU/User Management/EK-2667 Sign in</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
