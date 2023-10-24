4558e36587e9f602c7fac4bd3df74f35b7f757bd
samarir <samarir@masaratapp.com>
2023-10-24 14:51:26 +0000

    56th commit

--- Test Suites/User Management.ts
+++ Test Suites/User Management.ts
@@ -12,24 +12,24 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>d8ece6f3-f2d4-414a-bbc0-3a63518560bf</testSuiteGuid>
    <testCaseLink>
-      <guid>febd10fb-e0d2-4ce8-84d0-02c38c764729</guid>
+      <guid>1d271993-5c4a-4ac0-a59e-a587a785d76e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/IS/User Managemenet/Privacy Terms and Conditions</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IS/User Managemenet/EK-7997 Katalon - Terms  Conditions  Privacy policy</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8344d2e0-b27a-4c1c-95a6-eb4240e2bb9c</guid>
+      <guid>d72bdda0-8627-47b9-8a2b-ff7738a09b4e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/IS/User Managemenet/Restore Password</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IS/User Managemenet/EK-7996 IS Android - Restore Password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b86c9e76-fa19-43fa-b7ec-bd50afcc8f98</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
-      <testCaseId>Test Cases/IS/User Managemenet/10loginTestAttempts</testCaseId>
+      <testCaseId>Test Cases/IS/User Managemenet/EK-7998 IS Android- Disable after 10 login attempts</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
@@ -40,24 +40,24 @@
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8eeb1717-2d23-44ba-9dd6-29b350731c9a</guid>
+      <guid>baf0eede-238e-4bde-a4f7-f44bb7624ad7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/IS/User Managemenet/Logout</testCaseId>
+      <testCaseId>Test Cases/IS/User Managemenet/EK-7994 Katalon - Logout</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>5256675d-35a4-4a6f-bbb2-e1108ca92753</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
-      <testCaseId>Test Cases/IS/User Managemenet/Login as user with multiple sites</testCaseId>
+      <testCaseId>Test Cases/IS/User Managemenet/EK-7999 IS Android- Login as a user with multiple sites</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>41096717-2077-43b6-a3dd-725140a489ad</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
-      <testCaseId>Test Cases/IS/User Managemenet/Auto-logout</testCaseId>
+      <testCaseId>Test Cases/IS/User Managemenet/EK-7995 IS Android - Auto Logout</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
