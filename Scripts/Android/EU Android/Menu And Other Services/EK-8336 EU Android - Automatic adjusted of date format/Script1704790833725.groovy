efe4498907f659826c790de324529dc7d1d35fd2
samarir <samarir@masaratapp.com>
2024-01-30 11:09:35 +0100

    89th commit

--- Scripts/Android/EU Android/Menu And Other Services/EK-8336 EU Android - Automatic adjusted of date format/Script1704790833725.groovy
+++ Scripts/Android/EU Android/Menu And Other Services/EK-8336 EU Android - Automatic adjusted of date format/Script1704790833725.groovy
@@ -21,7 +21,7 @@ CucumberKW.runFeatureFile('Include/features/EK-8336 EU Android - Automatic adjus
 
 //String date = Mobile.getText(findTestObject('Object Repository/Android/IS/Menu and other services/Automatic adjustment of Date format/android.widget.TextView - 27072023'), 30, FailureHandling.STOP_ON_FAILURE)
 //Mobile.verifyMatch(date, '[0-9]{2}/[0-9]{2}/[0-9]{4}', true, FailureHandling.STOP_ON_FAILURE)
-WebUI.callTestCase(findTestCase('EU Android/User Management/EK-8286 EU Android - Sign-in'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Android/EU Android/User Management/EK-8286 EU Android - Sign-in'), [:], FailureHandling.STOP_ON_FAILURE)
 
 Mobile.tap(findTestObject('Android/Patient Directory/Left Menu'), 30)
 
