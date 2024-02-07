07351461e8a856b2fed726edb5ed0ea6d0576dee
samarir <samarir@masaratapp.com>
2024-02-07 12:31:40 +0100

    96th commit

--- Scripts/IOS/User Management/EK-8491 IS iPhone - Sign in/Script1704795534182.groovy
+++ Scripts/IOS/User Management/EK-8491 IS iPhone - Sign in/Script1704795534182.groovy
@@ -21,12 +21,17 @@ CucumberKW.runFeatureFile('Include/features/EK-8491 IS iPhone - Sign in.feature'
 
 Mobile.startApplication(GlobalVariable.LoginCreds.IPA, true)
 
-Mobile.setText(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeTextField - username'), GlobalVariable.LoginCreds.get('username'), 30)
+Mobile.tap(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeButton - Allow'), 30)
 
-Mobile.setText(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeSecureTextField - Passcode'), GlobalVariable.LoginCreds.get('password'), 30)
+Mobile.setText(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeTextField - username'), GlobalVariable.LoginCreds.get(
+        'username'), 30)
+
+Mobile.setText(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeSecureTextField - Passcode'), 
+    GlobalVariable.LoginCreds.get('password'), 30)
 
 Mobile.tap(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeOther - SIGN IN'), 30)
 
 Mobile.tap(findTestObject('IOS/User Management/Sign In/XCUIElementTypeOther - Katalon'), 30)
 
 WebUI.delay(10)
+
