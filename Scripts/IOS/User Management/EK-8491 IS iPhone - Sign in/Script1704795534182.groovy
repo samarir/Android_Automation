9188402ee6ef5ee0cf780e7072a5c608fa456f08
samarir <samarir@masaratapp.com>
2024-04-30 09:58:13 +0100

    128th commit

--- Scripts/IOS/User Management/EK-8491 IS iPhone - Sign in/Script1704795534182.groovy
+++ Scripts/IOS/User Management/EK-8491 IS iPhone - Sign in/Script1704795534182.groovy
@@ -19,9 +19,9 @@ import org.openqa.selenium.Keys as Keys
 
 CucumberKW.runFeatureFile('Include/features/EK-8491 IS iPhone - Sign in.feature')
 
-//Mobile.startApplication(GlobalVariable.LoginCreds.IPA, true)
+Mobile.startApplication(GlobalVariable.LoginCreds.IPA, true)
 
-Mobile.tap(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeButton - Allow'), 30)
+//Mobile.tap(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeButton - Allow'), 30)
 
 Mobile.setText(findTestObject('Object Repository/IOS/User Management/Sign In/XCUIElementTypeTextField - username'), GlobalVariable.LoginCreds.get(
         'username'), 30)
